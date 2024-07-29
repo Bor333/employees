f58417d0224f363c71ac1b41c5bc465cef26ac9c
Bor333 <mugen999999999@gmail.com>
2024-07-29 11:37:42 +0700

    READ

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-function getDb()
-{
-    $mysqli = new mysqli('127.0.0.1', 'root', '', 'employees');
-    return $mysqli;
-}
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
-include "config.php";
 
-$mysqli = getDb();
+$mysqli = new mysqli('127.0.0.1', 'root', '', 'employees');
 
-$result = $mysqli->query("SELECT * FROM `employees`")->fetch_all();
+$result = $mysqli->query("SELECT * FROM `employees`");
 
-foreach ($result as $item) {
-    var_dump($item);
-    echo "<br>";
-}
-
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Список сотрудников</h1>
+<?php foreach ($result as $item): ?>
+<p>Имя: <?= ($item['name']) ?></p>
+<p>Фамилия: <?= ($item['surname']) ?></p>
+<p>Должность: <?= ($item['position']) ?></p>
+<p>Зарплата: <?= ($item['salary']) ?></p>
+    <hr>
+<?php endforeach; ?>
+</body>
+</html>
