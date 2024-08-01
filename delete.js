b696be32dace201f6e371a1243c42855f647725c
Bor333 <mugen999999999@gmail.com>
2024-08-01 19:52:08 +0700

    delete from db work

--- delete.js
+++ delete.js
@@ -6,18 +6,17 @@ deleteButtonArr.forEach((deleteButton) => {
             event.preventDefault();
 
             (async () => {
-                console.log(this.dataset.id)
-                // const id = document.getElementById('delete-id').value;
-                // const response = await fetch('add.php', {
-                //     method: 'POST',
-                //     headers: new Headers({
-                //         'Content-Type': 'application/json'
-                //     }),
-                //     body: JSON.stringify({
-                //         id: id,
-                //
-                //     }), // body data type must match "Content-Type" header
-                // });
+                const id = this.dataset.id;
+                console.log(id)
+                const response = await fetch('delete.php', {
+                    method: 'POST',
+                    headers: new Headers({
+                        'Content-Type': 'application/json'
+                    }),
+                    body: JSON.stringify({
+                        id: id
+                    }), // body data type must match "Content-Type" header
+                });
                 // const answer = await response.json();
                 //
                 //  console.log(answer);
--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
+//header("Content-type: application/json");
 $data = json_decode(file_get_contents('php://input'));
 
 $id = $data->id;
 
 
-//header("Content-type: application/json");
 //echo json_encode($response);
 $mysqli = new mysqli('127.0.0.1', 'root', '', 'employees');
 
